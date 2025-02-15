10873c85772c9a2e7474f5a0a83a87e0e0a014bc
SubhashSonu <76140347+SubhashSonu@users.noreply.github.com>
2025-02-15 08:39:31 +0000

    make changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using node js ,not browser
+
+console.log(3+3)
+// code readablity should be high
+console.log("Subhash")
+
+let name = "Subh"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+// boolean =>true/false
+// null
