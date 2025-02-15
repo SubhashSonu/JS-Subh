443000b8597ecbb0a215baa9568f721b52a3cc09
SubhashSonu <76140347+SubhashSonu@users.noreply.github.com>
2025-02-15 09:19:51 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,9 +9,18 @@ console.log("Subhash")
 let name = "Subh"
 let age = 20
 let isLoggedIn = false
+let state;
 
 // number => 2 to power 53
 //bigint 
 // string => ""
 // boolean =>true/false
 // null
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
