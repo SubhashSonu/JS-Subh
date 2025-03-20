b485db788f6fd2a0895af71415bd0100bcddd6e4
SubhashSonu <76140347+SubhashSonu@users.noreply.github.com>
2025-03-20 20:05:49 +0000

    Javascript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,12 +3,54 @@ const b = 20
 var c = 30 // global scope
 
 if(true){
-   let a = 10  // a = 10
+   let a = 10  // a = 10  global value of doesnot changes
    const b = 20
-   c = 30
-   console.log("INNER:",a)
+   c = 30          // global value is modified after reintialising c
+//    console.log("INNER:",a)
 }
 
-console.log("Outer:",a);
+// console.log("Outer:",a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "subhash"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)
+
+    two()
+}
+
+
+// one()
+
+if(true){
+    const username = "subhash"
+    if (username === "subhash"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+
+}
+
+// console.log(username);
+
+// ++++++++++++ interesting ++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
